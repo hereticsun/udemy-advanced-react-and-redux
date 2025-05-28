490a87d82871b4bd951639dd162481f8eb611187
Azlan <azlan@cuttilan.co.uk>
2025-05-28 10:38:49 +0100

    16-tricking-react-with-jsdom

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -3,5 +3,9 @@ import ReactDOM from 'react-dom';
 import App from '../App';
 
 it('shows a comment box', () => {
-    
+    const div = document.createElement('div');
+
+    ReactDOM.render(<App />, div);
+
+    ReactDOM.unmountComponentAtNode(div);
 });
