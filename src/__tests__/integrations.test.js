12294feb43a4a4431af4bda84399fd061741a19a
Azlan <azlan@cuttilan.co.uk>
2025-05-29 11:09:05 +0100

    57-fixing-a-broken-test

--- src/__tests__/integrations.test.js
+++ src/__tests__/integrations.test.js
@@ -4,5 +4,10 @@ import Root from 'Root';
 import App from 'components/App';
 
 it('can fetch a list of comments and display them', () => {
-
+    //Attempt to render the *entire* app
+    const wrapped = mount(
+        <Root>
+            <App />
+        </Root>
+    );
 });
