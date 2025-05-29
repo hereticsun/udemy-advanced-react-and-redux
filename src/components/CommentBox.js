2f427f1cf0aa2642218b29fcb077e38465684631
Azlan <azlan@cuttilan.co.uk>
2025-05-29 13:06:08 +0100

    77 Placing Reusable Logic

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -5,24 +5,6 @@ import * as actions from 'actions';
 class CommentBox extends Component {
     state = { comment: '' };
 
-    componentDidMount() {
-        this.shouldNavigateAway();
-    }
-
-    componentDidUpdate() {
-        this.shouldNavigateAway();
-    }
-
-    shouldNavigateAway() {
-        if (!this.props.auth) {
-            this.props.history.push('/');
-        }
-    }
-
-    handleChange = ( event ) => {
-        this.setState({comment: event.target.value});
-    };
-
     handleSubmit = ( event ) => {
         event.preventDefault();
 
@@ -46,8 +28,5 @@ class CommentBox extends Component {
     }
 };
 
-function mapStateToProps(state) {
-    return { auth: state.auth };
-};
 
-export default connect(mapStateToProps, actions)(CommentBox);
+export default connect(null, actions)(CommentBox);
--- src/components/requireAuth.js
+++ src/components/requireAuth.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 export default (ChildComponent) => {
     class ComposedComponent extends Component {
+    componentDidMount() {
+        this.shouldNavigateAway();
+    }
+
+    componentDidUpdate() {
+        this.shouldNavigateAway();
+    }
+
+    shouldNavigateAway() {
+        if (!this.props.auth) {
+            this.props.history.push('/');
+        }
+    }
+
+    handleChange = ( event ) => {
+        this.setState({comment: event.target.value});
+    };
+
         render() {
             return <ChildComponent />;
         };
     };
 
-    return ComposedComponent;
+    function mapStateToProps(state) {
+        return { auth: state.auth };
+    };
+    
+    return connect(mapStateToProps)(ComposedComponent);
 };
